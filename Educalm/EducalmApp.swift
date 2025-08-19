acbb8cdc3337847040264fd967d2efad2ac422f7
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-08-19 14:59:04 +0800

    Initial Commit

---
+++ Educalm/EducalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducalmApp.swift
+//  Educalm
+//
+//  Created by Adon Omeri on 19/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct EducalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
